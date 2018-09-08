fe891f5388bc6d231153a48b58787518fbaa3456
rubypruneda <rubypruneda@outlook.com>
2018-09-08 15:30:31 -0600

    user input is being saved in data function

--- src/components/Favs.vue
+++ src/components/Favs.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="hello">
     <div class='holder'>
+    <form @submit.prevent="addFav">
+      <input type="text" placeholder="Enter your favorite.."  v-model="fav">
+    </form>
+    <p>{{fav}}</p>
       <ul>
-        <li v-for="(info, i) in favs" :key="i">{{i}}. {{info.favorite}}</li>
+        <li v-for="(info, i) in favs" :key="i"> {{info.fav}}</li>
       </ul>
       <p>Here are you favorites!</p>
     </div>
@@ -14,19 +18,33 @@ export default {
   name: 'Favs',
   data() {
     return {
+      fav: '',
      favs: [
-       {'favorite': 'Family'},
-       {'favorite': 'Pizza'},
-     ],
-     showAlert: true,
-     showClasses: true,
+       {'fav': 'Family'},
+       {'fav': 'Pizza'}
+     ]
     }
+  },
+  methods : {
+      addFav(){
+          this.favs.push({fav: this.fav});
+          this.fav = '';
+      }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #323333;
+    color: #687F7F;
+  }
+
 .holder {
     background: #fff;
   }
